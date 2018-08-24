686a6b79364078bcd089b762a0ca07d725ca5908
Moises P. Sena <moisespsena@gmail.com>
2018-08-24 09:45:17 -0300

    Update admin_plugin

--- plugin.go
+++ plugin.go
@@ -1,7 +1,7 @@
 package geocode
 
 import (
-	"github.com/aghape/admin/adminplugin"
+	"github.com/aghape-pkg/admin"
 	"github.com/aghape/db"
 	"github.com/aghape/plug"
 )
@@ -9,14 +9,14 @@ import (
 type Plugin struct {
 	db.DBNames
 	plug.EventDispatcher
-	adminplugin.AdminNames
+	admin_plugin.AdminNames
 }
 
 func (p *Plugin) OnRegister() {
 	db.Events(p).DBOnInitE(func(e *db.DBEvent) error {
 		return Migrate(e.DB)
 	})
-	p.AdminNames.OnInitResources(p, func(e *adminplugin.AdminEvent) {
+	admin_plugin.Events(p).InitResources(func(e *admin_plugin.AdminEvent) {
 		InitResource(e.Admin)
 	})
 }
